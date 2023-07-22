6bcf1e8aaa71a3d5383459c8921ebaa49636a456
unknown <rai2889@gmail.com>
2023-07-22 14:54:47 +0530

    responsiveness of product page

--- frontend/src/Componants/ProductList.jsx
+++ frontend/src/Componants/ProductList.jsx
@@ -43,6 +43,18 @@ export default function ProductList() {
 const DIV= styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  @media (max-width: 800px) {
+    display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  }
+  @media (max-width: 500px) {
+    display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 300px) {
+    display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  }
 `;
 const SIDEBAR=styled.div`
   display: flex;
