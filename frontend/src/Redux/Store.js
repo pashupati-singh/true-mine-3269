b9da0223e2776e2959beb92ce7af147546d35041
pashupati-singh <119448686+pashupati-singh@users.noreply.github.com>
2023-07-23 12:28:48 +0530

    update

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -1,11 +1,11 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 
-import {reducer as productReducer} from "./Products/reducer"
+import { reducer as productReducer } from "./Products/reducer";
 import thunk from "redux-thunk";
+import { reducer as authReducer } from "./Auth/reducer";
+const rootReducer = combineReducers({
+  productReducer,
+  authReducer,
+});
 
-const rootReducer=combineReducers({
-
-    productReducer,
-})
-
-export const store=legacy_createStore(rootReducer,applyMiddleware(thunk))
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
