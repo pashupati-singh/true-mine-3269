a2be937bb2caf078f56e63ed9921f83755ab8d39
omkarSw2 <omkar21143@gmail.com>
2023-07-23 23:03:13 +0530

    omkar

--- frontend/src/Componants/HomeTabs.jsx
+++ frontend/src/Componants/HomeTabs.jsx
@@ -1,19 +1,22 @@
 import {
   Center,
+  SimpleGrid,
   Tab,
   TabList,
   TabPanel,
   TabPanels,
   Tabs,
 } from "@chakra-ui/react";
+
 import React from "react";
+import HomeCards from "./HomeCards";
 
 export default function HomeTabs() {
   return (
     <div>
       <Tabs>
         <Center>
-          <TabList >
+          <TabList>
             <Tab _selected={{ color: "green", fontWeight: "600" }}>One</Tab>
             <Tab _selected={{ color: "green", fontWeight: "600" }}>Two</Tab>
             <Tab _selected={{ color: "green", fontWeight: "600" }}>Three</Tab>
@@ -21,7 +24,11 @@ export default function HomeTabs() {
         </Center>
         <TabPanels>
           <TabPanel>
-            <p>one!</p>
+            <SimpleGrid columns={{ base: 1, md: 4 }} spacing={10}>
+              {Seeds?.map((item) => (
+                <HomeCards key={item.id} {...item} />
+              ))}
+            </SimpleGrid>
           </TabPanel>
           <TabPanel>
             <p>two!</p>
@@ -34,3 +41,53 @@ export default function HomeTabs() {
     </div>
   );
 }
+
+const Seeds = [
+  {
+    _id: "64baee5085ea3ee2551692b2",
+    primary_image:
+      "https://gardenguru.in/cdn/shop/products/Marigold-Nana-Patula-Yellow-Seeds_grande.jpg?v=1629235784",
+    alternative_image:
+      "https://gardenguru.in/cdn/shop/products/Marigold-Nana-Patula-Yellow-Seeds_grande.jpg?v=1629235784",
+    title: "Marigold Nana Patula Yellow Seeds",
+    price: 80,
+    old_price: "120",
+    category: "seeds",
+    type: "flower_seeds",
+  },
+  {
+    _id: "64baee5085ea3ee2551692b5",
+    primary_image:
+      "https://gardenguru.in/cdn/shop/products/MarigoldWhite_grande.jpg?v=1645012850",
+    alternative_image:
+      "https://gardenguru.in/cdn/shop/products/MarigoldWhite_grande.jpg?v=1645012850",
+    title: "F1 Marigold vanilla White seeds",
+    price: 105,
+    category: "seeds",
+    type: "flower_seeds",
+  },
+  {
+    _id: "64baee5085ea3ee2551692b9",
+    primary_image:
+      "https://gardenguru.in/cdn/shop/products/434_grande.jpg?v=1627761555",
+    alternative_image:
+      "https://gardenguru.in/cdn/shop/products/434_grande.jpg?v=1627761555",
+    title: "Cosmos Sensation Mixed Seeds",
+    price: 105,
+
+    category: "seeds",
+    type: "flower_seeds",
+  },
+  {
+    _id: "64baee5085ea3ee2551692bc",
+    primary_image:
+      "https://gardenguru.in/cdn/shop/products/633-Strawberry-Alpine-Fragaria_grande.jpg?v=1621932067",
+    alternative_image:
+      "https://gardenguru.in/cdn/shop/products/633-Strawberry-Alpine-Fragaria_grande.jpg?v=1621932067",
+    title: "Strawberry Alpine Fragaria",
+    price: 140,
+
+    category: "seeds",
+    type: "fruit_seeds",
+  },
+];
