3a296ab9a42953c8087016089a8fca616a8986bb
pashupati-singh <119448686+pashupati-singh@users.noreply.github.com>
2023-07-24 12:54:50 +0530

    cart

--- frontend/src/Routes/Main.js
+++ frontend/src/Routes/Main.js
@@ -2,13 +2,13 @@ import React from "react";
 import { Route } from "react-router";
 import { Routes } from "react-router-dom";
 import ProductList from "../Componants/ProductList";
-import { Login } from "../Pages/Login" 
-
+import { Login } from "../Pages/Login";
 import Home from "../Pages/Home";
 import SingleProductPage from "../Pages/SingleProductPage";
-import Cart from "../Componants/Cart";
+// import Cart from "../Componants/Cart";
 import Signup from "../Pages/SignUp";
 import { Payment } from "../Pages/Payment";
+import CartPage from "../Componants/CartComponent";
 
 const MainRoutes = () => {
   return (
@@ -17,10 +17,11 @@ const MainRoutes = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="/products" element={<ProductList />}></Route>
         <Route path="/products/:id" element={<SingleProductPage />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/cart" element={<CartPage />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/payment" element={<Payment />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
+        {/* <Route path="/payment" element={<Payment />} /> */}
       </Routes>
     </>
   );
